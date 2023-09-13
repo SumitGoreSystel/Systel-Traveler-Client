f515e2e197f32aa0b8cddd3b7fbaba44c7a98542
SumitGoreSystel <139208351+SumitGoreSystel@users.noreply.github.com>
2023-09-13 12:57:22 +0530

    dynamic table with filtered data

--- src/app/shared/color-table/color-table.component.ts
+++ src/app/shared/color-table/color-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-color-table',
@@ -6,14 +6,25 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./color-table.component.scss']
 })
 export class ColorTableComponent{
+  WCColors = ["c584d3", "a084d2", "60a5e8", "60d9d9", "5ce7a1", "aae272", "fce153", "f8c459", "febc5a", "eb8a5b"];
+
   @Input() columns: string[] = [];
   @Input() tableData:any[]=[];
+  @Input() enableCheckboxColumn: boolean = false;
+  @Input() checkboxColumnLabel: string = 'Select';
+  @Input() enableActionColumn: boolean = false;
+  @Input() actionColumnLabel: string = 'Actions';
+  @Input() actionOptions: any[] = [];
+  @Input() checkboxStateBinding: string = 'isActive';
 
+  @Output() checkboxChange: EventEmitter<{ row: any, isChecked: boolean }> = new EventEmitter<{ row: any, isChecked: boolean }>();
 
-  getRowValue(row: any, columnIndex: number): any {
-    const rowKeys = Object.keys(row);
-    const keyAtIndex = rowKeys[columnIndex];
-    return row[keyAtIndex];
+  onCheckboxChange(row: any) {
+    const isChecked = row[this.checkboxStateBinding];
+    this.checkboxChange.emit({ row, isChecked });
   }
+
+
+  
 
 }
--- src/app/traveler-main/User/user/user.component.ts
+++ src/app/traveler-main/User/user/user.component.ts
@@ -11,10 +11,29 @@ export class UserComponent implements OnInit {
   constructor(private _userMasterService:UserMasterService){}
 
   UserList:UserMasterDTO[]=[];
+  filteredUsers:any
 
   ngOnInit(): void {
     this._userMasterService.getUsersList().subscribe((userlist:UserData)=>{
       this.UserList = userlist.users
+       this.filteredUsers = userlist.users.map(user => ({
+        Name: `${user.firstName}${user.middleName ? ' ' + user.middleName : ''} ${user.lastName}`,
+        Designation: user.designation,
+        Phone: user.mobileNo,
+        Email: user.emailId,
+        Username: user.username,
+        CreatedBy: user.createdBy,
+        AssignedWorkcenter: user.assignedWC,
+        Active:user.isActive
+    }));
     })
+  }
+
+
+
+  onCheckboxStatusChange(event: { row: any, isChecked: boolean }) {
+    const { row, isChecked } = event;
+    // Handle the checkbox status change here
+    console.log('Checkbox status changed:', row, 'isChecked:', isChecked);
   }
 }
